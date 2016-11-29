426dd1d210b7648a19f47450975db89518487242
Stefan Abramiuk <s.abramiuk@gmail.com>
2016-11-29 22:06:13 +0100

    Googole staff

--- js/gOOgle.js
+++ js/gOOgle.js
@@ -4,8 +4,7 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-80141717-1', 'auto');
-  ga('require', 'linkid');
+  ga('create', 'UA-80141717-2', 'auto');
   ga('send', 'pageview');
 /* END */
 
